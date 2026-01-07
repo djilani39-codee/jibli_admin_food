c3755bbf87e67e942985bce8fb25e6cd6e9a5c10
Djilani <djilanibenhacine5@.com>
2026-01-07 15:01:08 +0100

    Initial commit - notfication

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,31 @@
 ﻿import UIKit
 import Flutter
+import FirebaseCore
+
+@UIApplicationMain
+@objc class AppDelegate: FlutterAppDelegate {
+  
+  override func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+  ) -> Bool {
+    
+    // 1. تهيئة Firebase
+    FirebaseApp.configure()
+    
+    // 2. تسجيل الإضافات
+    GeneratedPluginRegistrant.register(with: self)
+    
+    // 3. ضبط مفوض الإشعارات (FlutterAppDelegate يتولى ذلك عادةً)
+    if #available(iOS 10.0, *) {
+      UNUserNotificationCenter.current().delegate = self
+    }
+    
+    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+}
+import UIKit
+import Flutter
 import FirebaseCore // استيراد أساس فايربيز
 import UserNotifications
 
