7e4624bbbc9468cea4ed2dfcffe8830b2ef996e3
Djilani <djilanibenhacine5@.com>
2026-01-07 10:17:34 +0100

    Initial commit - notfication

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,7 +1,6 @@
 import UIKit
 import Flutter
-import Firebase
-import UserNotifications
+import FirebaseCore // استيراد أساس فايربيز
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -9,23 +8,17 @@ import UserNotifications
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    // 1. تهيئة فايربيز قبل أي شيء آخر
     FirebaseApp.configure()
+    
+    // 2. تسجيل إضافات فلاتر (تلقائياً)
     GeneratedPluginRegistrant.register(with: self)
-
-    UNUserNotificationCenter.current().delegate = self
-    application.registerForRemoteNotifications()
+    
+    // 3. تعيين المفوض للإشعارات (مدمج في FlutterAppDelegate)
+    if #available(iOS 10.0, *) {
+      UNUserNotificationCenter.current().delegate = self as? UNUserNotificationCenterDelegate
+    }
 
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
-
-  override func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
-    Messaging.messaging().apnsToken = deviceToken
-    super.application(application, didRegisterForRemoteNotificationsWithDeviceToken: deviceToken)
-  }
-}
-
-extension AppDelegate: UNUserNotificationCenterDelegate {
-  func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
-    completionHandler([.alert, .badge, .sound])
   }
 }
